bce7fb93d7db7707a39de626a22f264897dae4d3
QuantaShade <developer.mustafa1st@gmail.com>
2026-05-20 07:41:16 -0700

    change

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from .api.crud.index import get
+from django.shortcuts import render, redirect
+from .models import Content
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'app/home.html', get(request))
+    content = Content.objects.all().order_by("-postedAt")
+    context = {
+        'data': content
+    }
+    if request.method == "POST":
+        title = request.POST.get('title')
+        img = request.POST.get('img')
+        content = Content.objects.create(
+            title = title,
+            img = img
+        )
+        content.save()
+        return redirect("/")
+    return render(request, 'app/home.html', context)
 
 def about(request):
     return render(request, 'app/about.html')
